3dd808a487ecf24d32c5de3e605dd58544e19b33
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-19 18:22:37 +0800

    split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {build, defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'react-dom': ['react-dom'],
+                    'antd': ['antd'],
+                    // 'antd-pro': ['@ant-design/pro-components'],
+                }
+            }
+        }
+    },
   plugins: [react()],
 })
